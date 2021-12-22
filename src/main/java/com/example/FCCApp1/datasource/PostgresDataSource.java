c7a8842581c86c2fb9e6cbeb74f09e608d342f72
abhilashgd <anrewgd@gmail.com>
2021-12-22 22:17:44 +0530

    postgresImpl

---
+++ src/main/java/com/example/FCCApp1/datasource/PostgresDataSource.java
@@ -0,0 +1,27 @@
+package com.example.FCCApp1.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class PostgresDataSource {
+
+    @Primary
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource dataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().type(HikariDataSource.class)
+                .build();
+    }
+}
